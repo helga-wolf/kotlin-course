3ddd10f536b2ca46e9f666a8b4f5cd2e304f3a60
helga-wolf <helgawolf18@gmail.com>
2024-09-19 00:02:43 +0300

    конспект 5 урока

---
+++ src/main/kotlin/org/helgawolf/kotlincourse/lessonsummaries/lesson5.kt
@@ -0,0 +1,25 @@
+package org.helgawolf.kotlincourse.lessonsummaries
+
+fun main() {
+    val sum = 10 + 5
+    val diff = 10 - 5
+    val product = 10 * 5
+    val quotient = 10 / 5 //просто деление
+    val remainder = 10 % 3
+    val isEqual = (5 == 5)
+    val isNotEqual = (5 != 3)
+    val isGreater = (5 > 3)
+    val isLesser = (2 < 3)
+    val isGreaterOrEqual = (5 >= 5)
+    val isLesserOrEqual = (5 <= 7)
+    var number = 5
+    number = number + 5
+    number -= 1
+
+    println(number)
+}
+
+//приоритет логич операций:
+//        НЕ
+//        И
+//        ИЛИ

34690bfab7832460a6c6d852c500a3069ba0f997
helga-wolf <helgawolf18@gmail.com>
2024-10-03 10:02:30 +0300

    с др :) - дз 9 урока

---
+++ src/main/kotlin/org/helgawolf/kotlincourse/lesson9/homework9.kt
@@ -0,0 +1,9 @@
+package org.helgawolf.kotlincourse.lesson9
+
+//запускать осторожно! комп повешается!
+fun main() {
+
+    while (true) {
+        println("Happy belated Bday, Alexander!")
+    }
+}

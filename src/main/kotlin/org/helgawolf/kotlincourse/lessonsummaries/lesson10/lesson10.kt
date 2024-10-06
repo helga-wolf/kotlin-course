6f8aeed1f42287a508588c8b7299657d148b5e1d
helga-wolf <helgawolf18@gmail.com>
2024-10-06 17:46:42 +0300

    конспект 10 урока

--- src/main/kotlin/org/helgawolf/kotlincourse/lessonsummaries/lesson10/lesson10.kt
+++ src/main/kotlin/org/helgawolf/kotlincourse/lessonsummaries/lesson10/lesson10.kt
@@ -1,40 +1,56 @@
 package org.helgawolf.kotlincourse.lessonsummaries.lesson10
 
 fun main() {
+    //массив - упорядоченная коллекция фиксированного размера, сод-я эл-ты одного типа
+    // если тип явно не указан - оперделится автом-ки
+    //из массива нельзя удалить или добавить значения - только заменить. как создался так и будет всегда одного размера
     val numbers: Array<Int> = arrayOf(1, 2, 3, 4, 5)
 
     val doubles: DoubleArray = doubleArrayOf(1.1, 2.2, 3.3)
 
-    val emptyArray = Array(5) { "" }
+    val emptyArray = Array(5) { "" } //- лямбда-выражение
+    val emptyArray666 = Array(5) { 1 } // массив из 5 элтов где каждый элт это единица
     val emptyNullableArray: Array<Int?> = arrayOfNulls<Int>(5)
+    //создать массив без ничего можно только если элементы бцдцт наллабл
+
 //массивы всегд изменяемы. списки - когда как
 
     val readOnlyList: List<String> = listOf("a", "b", "c")
+    //listOf создает неизм список - ни довавить ни заменить ни удалить элт
+    //но если там внутри модифицируемые обекты  - внутри них можно изменять
 
     val mutableList: MutableList<String> = mutableListOf("a", "b", "c")
+    //а вот это уже изменяемый список - как содержимое чяекк так и их колво
 
+    //множество - все элементы уникальны. если завести 2 одинак значения - второе будет проигнорировано
     val numbersSet: Set<Int> = setOf(1, 2, 3, 4, 5)
+val mutableNumbersSet: MutableSet<Int> = mutableSetOf(1,2,3,4,5)//множество изменяемое
 
-    mutableList.add("ui")
-    val set = setOf("k", "o", "t")
-    for(letter in set) {
+   //М Е Т О Д Ы
+
+    mutableList.add("ui")// add добавляет в конец списка
+    val set = setOf("k", "o", "t")// у множества НЕТ индексов. получить по индексу нельзя! можно только перебирать по очереди
+    for(letter in set) {//перебор набора по очереди начиная с 1-го
         println("| $letter |")
     }
-println(doubles[1])
+println(doubles[1])// получение элта по ииндексу - считаем начиная с 0
 
-    val list = listOf(32, 53,1, -76)
-    for (index in list.indices) {
+    val list = listOf(32, 53,1, -76)//работает тут не с самими элтами а с их индексами
+    for (index in list.indices) {//создается диапазон индексов от 0 до 3 в данном примере
         if (index == list.lastIndex) {
             println(list[index] + list[0])
+            println(list.last())//вернет последний индекс
         } else {
             println(list[index] + list[index + 1])
         }
     }
 
+    //while только для списков и массивов тк там есть индекс
+    //для сетов - for
     var index = list.lastIndex
     while (index >= 0) {
         println("`${list[index--]}`")
-    }
+    }// вывели все элты в обратном порядке в косых кавычках
 
 
     val array = Array (100) {0}
@@ -90,9 +106,9 @@ val set1 = setOf(1,2,3)
     for (elem in set1) {
         emptySet1.add(elem)
     }
-    println(emptySet)
-    emptySet = mutableSetOf(1)
-    println(emptySet)
+//    println(emptySet)
+//    emptySet = mutableSetOf(1)
+//    println(emptySet)
 }
 
 //fun findNumber (setNum: Set<Int>,num: Int): Boolean {
@@ -103,4 +119,3 @@ val set1 = setOf(1,2,3)
 //    }
 
 
-}
